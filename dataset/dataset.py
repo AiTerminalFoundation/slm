2b32f3d304ea825cd5c0d9393280e528423e8a09
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2026-01-19 20:19:50 +0000

    improve dataset code

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -3,20 +3,17 @@ from torch.utils.data import Dataset
 
 class NaturalLanguageToBashDataset(Dataset):
 
-    def __init__(self, natural_language_file, commands_file, transform=None):
+    def __init__(self, natural_language_file, commands_file):
         """
         Args:
             natural_language_file (str): path to text file with natural language prompts
             commands_file (str): path to text file with bash commands
-            transform (callable, optional): optional transform on a sample
         """
         with open(natural_language_file, "r", encoding="utf-8") as file:
             self.prompts = [line.strip() for line in file]
 
         with open(commands_file, "r", encoding="utf-8") as file:
             self.commands = [line.strip() for line in file]
-
-        self.transform = transform
 
     def __len__(self):
         return len(self.prompts)
@@ -29,9 +26,4 @@ class NaturalLanguageToBashDataset(Dataset):
             "prompt": self.prompts[idx],
             "command": self.commands[idx]
         }
-
-        if self.transform:
-            sample = self.transform(sample)
-
         return sample
-
