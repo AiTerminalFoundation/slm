d8ef7e213114d976d81a51494034853abf5e0fe6
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2026-01-28 14:17:47 +0000

    pre-tokenization

--- tokenization/tokenizer.rs
+++ tokenization/tokenizer.rs
@@ -1,15 +1,13 @@
 struct ByteLevelBPETokenizer {
     num_merges: u16,
     vocabulary: HashSet<Vec<u8>, u32>, // maps byte sequences to ids
-    merges: Vec<(Vec<u8>, Vec<u8>)>, // ordered list of merge operations
+    merges: Vec<(Vec<u8>, Vec<u8>)>,   // ordered list of merge operations
     byte_encoder: HashMap<u8, String>, // maps bytes to unicode characters
     byte_decoder: HashMap<String, u8>, // maps unicode characters to bytes
 }
 
 impl ByteLevelBPETokenizer {
-    fn new(corpus: str, num_merges: i8) -> Self {
-
-    }
+    fn new(corpus: str, num_merges: i8) -> Self {}
 
     fn train(&self, corpus: str) {
         // pre-tokenize corpus
@@ -23,12 +21,22 @@ impl ByteLevelBPETokenizer {
         let mut word_pairs = HashSet::new();
 
         for i in 0..(word.len() - 1) {
-
             let mut pair: [u8; 2] = [word[i], word[i + 1]];
 
             word_pairs.insert(pair)
         }
 
         word_pairs
+    }
+
+    /// Get the corpus as a string, split it by whitespace, and convert each word to a byte sequence.
+    /// This is the first easy implementation of a pre-tokenization just splitting words by spaces
+    /// instead of using a regex (that it will be the next step after all the LLM works decently)
+    fn pre_tokenize_corpus(&self, corpus: str) -> Vec<Vec<u8>> {
+        let words_by_space: Vec<str> = corpus.split_whitespace();
+        let words_as_bytes: Vec<Vec<u8>> = words_by_space
+            .iter()
+            .map(|word_as_characters| word_as_characters.as_bytes().to_vec())
+            .collect();
     }
 }
